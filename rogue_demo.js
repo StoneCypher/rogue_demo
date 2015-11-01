7ea61ccf28d5ca706397a7a33773e42a8e236b0e
John Haugeland <stonecypher@gmail.com>
2015-11-01 11:16:04 -0800

    Step 3: render map strings in array version

--- rogue_demo.js
+++ rogue_demo.js
@@ -1,3 +1,12 @@
+var str_map = '##+#########' +  // todo: remove player, creatures, objects
+            '\n#@....######' +
+            '\n#...R.##...#' +
+            '\n#.$...##.p.#' +
+            '\n###+####...#' +
+            '\n###....=..p#' +
+            '\n########...#' +
+            '\n############';
+
 var map = [ [ '#', '#', '+', '#', '#', '#', '#', '#', '#', '#', '#', '#' ], // todo: remove player, creatures, objects
             [ '#', '@', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#' ],
             [ '#', '.', '.', '.', 'R', '.', '#', '#', '.', '.', '.', '#' ],
@@ -15,10 +24,10 @@ var map = [ [ '#', '#', '+', '#', '#', '#', '#', '#', '#', '#', '#', '#' ], // t
 var RenderCell =              cell => '<td>' + cell.toString() + '</td>',
     RenderRow  =               row => '<tr>' + (row.map(RenderCell).join('')) + '</tr>',
     RenderMap  =           mapdata => '<table><tbody>' + (mapdata.map(RenderRow).join('')) + '</tbody></table>',
-    mapStringToGameMap = mapString => mapString.split('\n').map( row => row.split() );
+    mapStringToGameMap = mapString => mapString.split('\n').map( row => row.split('') );
 
 function onStart() {
-  document.body.innerHTML = RenderMap(map);
+  document.body.innerHTML = RenderMap(mapStringToGameMap(str_map));
 }
 
 window.onload = onStart;
